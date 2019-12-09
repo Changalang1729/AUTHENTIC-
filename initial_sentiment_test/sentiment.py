1e35e9ef8ec8cf16b54eb57f3a997215aae66795
brianzhao1 <brian.zhao@postmates.com>
2019-12-08 19:30:54 -0500

    Use compound score to check pos/neg sentiment
    Refactor the way we split passages into sentences, check compound scores of each sentence to generate decision.

--- initial_sentiment_test/sentiment.py
+++ initial_sentiment_test/sentiment.py
@@ -1,24 +1,34 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from nltk.tokenize import sent_tokenize
 from collections import Counter
 
 analyser = SentimentIntensityAnalyzer()
 
 def score_sentence(sentence):
     score = analyser.polarity_scores(sentence)
-    print("{:-<40} {}".format(sentence, str(score)))
+    # print("{:-<40} {}".format(sentence, str(score)))
     return Counter(score)
 
 def score_passage(passage):
-	sentence_array = [s.strip(' \n') for s in passage.split('.')]
-	total_score = 0
+	split_passage = sent_tokenize(passage)
+	sentence_array = [s.strip(' \n') for s in split_passage]
+	passage_length = len(''.join(s for s in sentence_array))
+	total_score = Counter()
+	pos = neg = 0
+
 	for sentence in sentence_array:
-		total_score += score_sentence(sentence)
-	print(total_score)
-	return total_score
+		sentence_score = score_sentence(sentence)
+		compound_score = sentence_score.get('compound', 0)
+		
+		if compound_score > 0:
+			pos += compound_score
+		elif compound_score < 0:
+			neg -= compound_score
+
+	return 'pos' if pos > neg else 'neg'
+
 
 fin = open('passage.in', 'r')
 article = fin.read()
 
-
-# print(article)
-score_passage(article)
+print(score_passage(article))
