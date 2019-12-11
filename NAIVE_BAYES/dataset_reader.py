1b96e250f362c87602333fc262376885440f5892
brianzhao1 <brian.zhao@postmates.com>
2019-12-10 23:00:11 -0500

    Refactor functions to predict liberal/conservative

--- NAIVE_BAYES/dataset_reader.py
+++ NAIVE_BAYES/dataset_reader.py
@@ -2,16 +2,16 @@ import sys
 import csv
 import os
 
-left_articles = os.listdir('./left')
+Right_articles = os.listdir('./Right')
 # right_articles = os.listdir('./right')
 csv.field_size_limit(sys.maxsize)
-for left in left_articles:
-    if left == '.DS_Store' or '.txt' in left:
+for Right in Right_articles:
+    if Right == '.DS_Store' or '.txt' in Right:
         continue
-    txt_file = os.path.join('./left', left[:-4] + '.txt')
-    print(left)
+    txt_file = os.path.join('./Right', Right[:-4] + '.txt')
+    print(Right)
     with open(txt_file, "w") as my_output_file:
-        with open(os.path.join('./left', left), encoding="latin-1") as my_input_file:
+        with open(os.path.join('./Right', Right), encoding="latin-1") as my_input_file:
             [ my_output_file.write(" ".join(row)+'\n') for row in csv.reader(my_input_file)]
     my_output_file.close()
-    os.remove(os.path.join('./left', left))
+    os.remove(os.path.join('./Right', Right))
